2788e5e0582b67ec3cb8deb761c4e401e48229e5
joshe <work.joshe@gmail.com>
2026-08-16 00:15:03 +0530

    fix: resolve mobile network error by removing hardcoded api-base meta tag

--- app.py
+++ app.py
@@ -20,7 +20,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "http://localhost",
+        "capacitor://localhost",
     ],
     allow_credentials=True,
     allow_methods=["*"],
